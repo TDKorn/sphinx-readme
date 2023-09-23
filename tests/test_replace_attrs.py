9ef47e02bf21b1860eb0ce403ddcf35c1c26c48e
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-09-23 00:20:53 -0400

    Update test_replace_attrs.py
    account for all acceptable preceding/trailing characters

--- tests/test_replace_attrs.py
+++ tests/test_replace_attrs.py
@@ -1,5 +1,5 @@
 import pytest
-from sphinx_readme.utils.rst import replace_attrs, get_all_xref_variants
+from sphinx_readme.utils.rst import replace_attrs, get_all_xref_variants, AFTER_XREF, BEFORE_XREF
 
 test_cases = []
 target = "Class.attribute"
@@ -41,19 +41,75 @@ def test_replace_attrs_within_sentence(xref, replaced):
 
 @pytest.mark.parametrize("xref", xrefs)
 def test_replace_attrs_no_preceding_whitespace(xref):
+    """Cross-references should not be replaced if they are preceded by any non-whitespace character
+    other than the :attr:`~.utils.rst.BEFORE_XREF` characters.
+    """
     input_str = f"The{xref} cross-ref"
     assert replace_attrs(input_str) == input_str
 
 
+@pytest.mark.parametrize("xref,replaced", test_cases)
+def test_replace_attrs_only_preceding_whitespace(xref, replaced):
+    input_str = f"   {xref} cross-ref"
+    expected_output = f"   {replaced} cross-ref"
+    assert replace_attrs(input_str) == expected_output
+
+
 @pytest.mark.parametrize("xref", xrefs)
 def test_replace_attrs_no_trailing_whitespace(xref):
+    """Cross-references should not be replaced if they are followed by any non-whitespace character
+    other than the :attr:`~.utils.rst.AFTER_XREF` characters.
+    """
     input_str = f"The {xref}cross-ref"
     assert replace_attrs(input_str) == input_str
 
 
-@pytest.mark.parametrize("xref,replaced_xref", test_cases)
-def test_replace_attrs_with_trailing_colon(xref, replaced_xref):
-    """A colon after a cross-reference should not stop it from being replaced"""
-    input_str = f"The {xref}:"
-    expected_output = f"The {replaced_xref}:"
+@pytest.mark.parametrize("xref,replaced", test_cases)
+def test_replace_attrs_only_trailing_whitespace(xref, replaced):
+    input_str = f"The {xref}   "
+    expected_output = f"The {replaced}   "
     assert replace_attrs(input_str) == expected_output
+
+
+@pytest.mark.parametrize("xref,replaced", test_cases)
+def test_replace_attrs_trailing_newline(xref, replaced):
+    input_str = f"The {xref}\n"
+    expected_output = f"The {replaced}\n"
+    assert replace_attrs(input_str) == expected_output
+
+
+# Redefine these constants since the ones in ``rst.py`` are escaped
+BEFORE_XREF = ":[{(/\"'-"
+AFTER_XREF = ":;!?,\"'/\\])}-"
+
+
+@pytest.mark.parametrize("xref,replaced_xref", test_cases)
+def test_replace_attrs_with_preceding_acceptable_chars(xref, replaced_xref):
+    """Cross-references should be replaced if they're preceded by any of the :attr:`~.rst.BEFORE_XREF` characters"""
+    for char in BEFORE_XREF:
+        input_str = rf"The {char}{xref}"
+        expected_output = f"The {char}{replaced_xref}"
+        assert replace_attrs(input_str) == expected_output
+
+
+@pytest.mark.parametrize("xref,replaced_xref", test_cases)
+def test_replace_attrs_with_trailing_acceptable_chars(xref, replaced_xref):
+    """Cross-references should be replaced if they're followed by any of the :attr:`~.rst.AFTER_XREF` characters"""
+    for char in AFTER_XREF:
+        input_str = rf"The {xref}{char}"
+        expected_output = f"The {replaced_xref}{char}"
+        assert replace_attrs(input_str) == expected_output
+
+
+@pytest.mark.parametrize("xref,replaced_xref", test_cases)
+def test_replace_attrs_surrounded_by_acceptable_chars(xref, replaced_xref):
+    """Cross-references preceded by any of the :attr:`~.rst.BEFORE_XREF` characters and followed by any
+    of the :attr:`~.rst.AFTER_XREF` characters should be replaced.
+
+    This test may be overkill.
+    """
+    for prefix in BEFORE_XREF:
+        for suffix in AFTER_XREF:
+            input_str = rf"The {prefix}{xref}{suffix}"
+            expected_output = f"The {prefix}{replaced_xref}{suffix}"
+            assert replace_attrs(input_str) == expected_output
