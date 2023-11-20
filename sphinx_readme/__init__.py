21f50e08eb51c8ab6a63a558011801b064a6dd3f
TDKorn <96394652+TDKorn@users.noreply.github.com>
2023-11-19 21:41:16 -0500

    Bump to ```v1.0.0```
    i think it finally supports most sphinx features

--- sphinx_readme/__init__.py
+++ sphinx_readme/__init__.py
@@ -10,7 +10,7 @@ from sphinx_readme.utils.git import get_repo_dir
 from sphinx_readme.parser import READMEParser
 
 
-__version__ = "v0.2.5"
+__version__ = "v1.0.0"
 
 
 def setup(app: Sphinx) -> Dict[str, Any]:
