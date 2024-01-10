2c57187796fd225055bc014852f4ae392be6e3da
TDKorn <96394652+TDKorn@users.noreply.github.com>
2024-01-10 01:43:32 -0500

    Add `build_sphinx` fixture to simplify tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,5 +1,4 @@
 import pytest
-from sphinx.testing.path import path
 from tests.helpers import assert_doctree_equal
 
 
@@ -14,20 +13,13 @@ from tests.helpers import assert_doctree_equal
     ({"readme_raw_directive": False}, "rubric_markup_list_table"),
     ({"readme_rubric_heading": "=", "readme_raw_directive": False}, "rubric_heading_list_table"),
 ])
-def test_rubrics(confoverrides, expected_filename, src_dir, app_params, make_app, get_generated_doctree, get_expected_doctree):
+def test_rubrics(confoverrides, expected_filename, app_params, build_sphinx, get_generated_doctree, get_expected_doctree):
     src_file = "rubric.rst"
-    filenames = [src_dir.joinpath(src_file)]
-    confoverrides["readme_src_files"] = src_file
-
-    args, kwargs = app_params
-    kwargs.update({
-        "confoverrides": confoverrides,
-        "srcdir": path(src_dir)
-    })
-    app = make_app(*args, **kwargs)
-    app.build(filenames=filenames)
-
+    app = build_sphinx(
+        src_file=src_file,
+        app_params=app_params,
+        confoverrides=confoverrides
+    )
     expected = get_expected_doctree(app, "rubric", expected_filename)
     generated = get_generated_doctree(app, src_file)
     assert_doctree_equal(generated, expected)
-
