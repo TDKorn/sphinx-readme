6701914539f76f30666a68caf4a91b53b61cfb1b
TDKorn <96394652+TDKorn@users.noreply.github.com>
2024-01-21 18:11:53 -0500

    Create `parse_doctree` test helper

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,6 +1,18 @@
-"""Helper functions for comparing doctrees - adopted from ``sphinx-contrib/restbuilder``"""
+from pathlib import Path
 from itertools import zip_longest
-from docutils.nodes import Text, Element, system_message
+from sphinx.testing import restructuredtext
+from sphinx.testing.util import SphinxTestApp
+from docutils.nodes import Text, Element, system_message, document
+
+
+def parse_doctree(filepath: Path, app: SphinxTestApp) -> document:
+    """Parses an RST file into a doctree"""
+    if not filepath.exists():
+        raise FileNotFoundError(
+            f"File doesn't exist: {filepath}"
+        )
+    rst = filepath.read_text(encoding='utf-8')
+    return restructuredtext.parse(app, rst, filepath.stem)
 
 
 def assert_doctree_equal(generated_doctree, expected_doctree):
