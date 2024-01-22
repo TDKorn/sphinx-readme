530124d79b16977a503cf88b8276989d771a6fe6
TDKorn <96394652+TDKorn@users.noreply.github.com>
2024-01-22 00:46:13 -0500

    Add more cross-reference targets to `test_module`

--- tests/test_package/test_module.py
+++ tests/test_package/test_module.py
@@ -1,14 +1,33 @@
+from functools import cached_property
+
+
+TEST_VARIABLE = 'test_value'  #: This is a variable docstring
+
+
 class TestClass:
 
     def __init__(self, *args, **kwargs):
         self.args = args
         self.kwargs = kwargs
+        self.test_attr = 0  #: This is an attribute docstring
 
     def test_method(self):
-        pass
+        return self.kwargs
+
+    @property
+    def test_property(self):
+        return self.args
+
+    @cached_property
+    def test_cached_property(self):
+        return self.test_attr * 0
+
+
+class TestException(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
 
 
 def test_function(*args, **kwargs):
     return args, kwargs
-
 
